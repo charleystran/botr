2da2810eb2a079ba163805b424f62f0f1bab4096
bertrandk <b.karerangabo@gmail.com>
2013-08-01 17:12:52 -0400

    Add logger module.

--- lib/botr.rb
+++ lib/botr.rb
@@ -1,3 +1,5 @@
 require 'botr/version'
 
+require 'botr/common/logger'
+
 require 'botr/http/http_response'
---
+++ lib/botr/common/logger.rb
@@ -0,0 +1,31 @@
+module BOTR
+
+	module Logger
+
+		def self.included(base)
+			base.extend(ClassMethods)
+		end
+
+		def logger=(log)
+            @logger = log
+        end
+
+        def logger
+            @logger ||= self.class.logger
+        end
+
+        module ClassMethods
+
+            def logger=(log)
+                @@logger = log
+            end
+
+            def logger
+                @@logger ||= nil
+            end
+
+        end
+
+	end
+
+end
