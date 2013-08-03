7703d1fa2478f25e86c5553c6d6cdffd1a25ab49
bertrandk <b.karerangabo@gmail.com>
2013-08-03 15:01:22 -0400

    Add authentication module.

---
+++ lib/botr/api/authentication.rb
@@ -0,0 +1,24 @@
+require 'digest/sha1'
+
+module BOTR
+	
+	module Authentication
+
+    	def signature(params = {})
+    		sorted_params = {}
+    		str_params = ""
+
+    		# Sort params by key (hashes maintain insertion order)
+    		params.keys.sort.each do |key|
+    			sorted_params[key] = params[key]
+    		end
+
+    		# URL encode params
+    		str_params = URI.encode_www_form(sorted_params)
+
+    		Digest::SHA1.hexdigest str_params + secret
+    	end
+
+	end 
+
+end
---
+++ spec/authentication_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+require 'botr/api/authentication'
+
+class DummyClient 
+
+	def secret
+		"uA96CFtJa138E2T5GhKfngml"
+	end
+
+end
+
+describe BOTR::Authentication do
+
+	before(:each) do
+  		@dummy_client = DummyClient.new
+  		@dummy_client.extend(BOTR::Authentication)
+	end
+
+	describe "#signature" do
+
+		it "should generature signature" do
+			params = {:text				=> "démo",
+					  :api_format		=> "xml",
+					  :api_key			=> "XOqEAfxj",
+					  :api_nonce		=> "80684843",
+					  :api_timestamp	=> "1237387851"}
+			@dummy_client.signature(params).should eql "fbdee51a45980f9876834dc5ee1ec5e93f67cb89"
+		end	
+	end
+
+end
