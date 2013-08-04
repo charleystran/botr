25d2840882b3c5253744f7cd422887ca1ae99cc3
bertrandk <b.karerangabo@gmail.com>
2013-08-04 16:32:16 -0400

    Remove proxy support.

--- lib/botr/http/http.rb
+++ lib/botr/http/http.rb
@@ -2,25 +2,6 @@ module BOTR
 
 	module HTTP
 
-		def self.included(base)
-        	base.extend(ClassMethods)
-        end
-
-        def proxy=(options = {})
-            @proxy = {}
-            options.each do |key, value|
-                if [:host, :port, :user, :pass].include?(key)
-                    @proxy[key] = value
-                else
-                    raise ArgumentError, "Unsupported option: #{key}"
-                end
-            end
-        end
-
-        def proxy
-            @proxy ||= self.class.proxy
-        end
-
         def http_backend=(backend)
             if backend.is_a?(String)
                 raise ArgumentError, "Invalid HTTP backend: #{backend}" unless backend =~ /^[A-Za-z_]+$/
@@ -55,7 +36,7 @@ module BOTR
                 if !instance_of?(BOTR::Base) && http_backend == self.class.http_backend && self.class.client
                     @client = self.class.client
                 else
-                    @client = http_backend.new(host: proxy[:host], port: proxy[:port], username: proxy[:user], password: proxy[:pass])
+                    @client = http_backend.new
                 end
             end
             @client
@@ -71,98 +52,79 @@ module BOTR
             client.post(api_url, params.merge(:api_signature => self.signature(params)))
         end
 
-        module ClassMethods
-
-        	def proxy=(options = {})
-                @@proxy = {}
-                options.each do |key, value|
-                    if [:host, :port, :user, :pass].include?(key)
-                        @@proxy[key] = value
-                    else
-                        raise ArgumentError, "Unsupported option: #{key}"
-                    end
-                end
+        def http_backend=(backend)
+            if backend.is_a?(String)
+                raise ArgumentError, "Invalid HTTP backend: #{backend}" unless backend =~ /^[A-Za-z_]+$/
+                file = backend.gsub(/([a-z])([A-Z])/, '\1_\2').gsub(/([A-Z])([A-Z][a-z])/, '\1_\2').downcase
+                require "botr/http/#{file}"
+                backend = BOTR.const_get(backend)
             end
-
-            def proxy
-                @@proxy ||= {}
-            end
-
-            def http_backend=(backend)
-                if backend.is_a?(String)
-                    raise ArgumentError, "Invalid HTTP backend: #{backend}" unless backend =~ /^[A-Za-z_]+$/
-                    file = backend.gsub(/([a-z])([A-Z])/, '\1_\2').gsub(/([A-Z])([A-Z][a-z])/, '\1_\2').downcase
-                    require "botr/http/#{file}"
-                    backend = BOTR.const_get(backend)
-                end
-                if backend.is_a?(Class)
-                    if backend.included_modules.include?(BOTR::HTTPBackend)
-                        @@http_backend = backend
-                        @@client = nil
-                    else
-                        raise ArgumentError, "Invalid HTTP backend: #{backend.name}"
-                    end
+            if backend.is_a?(Class)
+                if backend.included_modules.include?(BOTR::HTTPBackend)
+                    @@http_backend = backend
+                    @@client = nil
                 else
-                    if backend.class.included_modules.include?(BOTR::HTTPBackend)
-                        @@http_backend = backend.class
-                        @@client = backend
-                    else
-                        raise ArgumentError, "Invalid HTTP backend: #{backend.class.name}"
-                    end
+                    raise ArgumentError, "Invalid HTTP backend: #{backend.name}"
+                end
+            else
+                if backend.class.included_modules.include?(BOTR::HTTPBackend)
+                    @@http_backend = backend.class
+                    @@client = backend
+                else
+                    raise ArgumentError, "Invalid HTTP backend: #{backend.class.name}"
                 end
             end
+        end
 
-            def http_backend
-	            require 'botr/http/http_backend'
-	            @@http_backend ||= BOTR::HTTPBackend  
+        def http_backend
+            require 'botr/http/http_backend'
+            @@http_backend ||= BOTR::HTTPBackend  
+        end
+
+        def client
+            @@client ||= nil
+            unless @@client
+                @@client = http_backend.new
             end
+            @@client
+        end
 
-            def client
-                @@client ||= nil
-                unless @@client
-                    @@client = http_backend.new(host: proxy[:host], port: proxy[:port], username: proxy[:user], password: proxy[:pass])
-                end
-                @@client
-            end
+        def get_request(options = {})
+            params = options.dup
 
-            def get_request(options = {})
-                params = options.dup
+            http      = params.delete(:client)        || client
+            url       = params.delete(:api_url)       || api_url
+            format    = params.delete(:api_format)    || api_format
+            key 	  = params.delete(:api_key)       || api_key
+            timestamp = params.delete(:api_timestamp) || api_timestamp
+            nonce     = params.delete(:api_nonce)     || api_nonce
+            secret    = params.delete(:secret_key)    || secret_key
 
-                http      = params.delete(:client)        || client
-                url       = params.delete(:api_url)       || api_url
-                format    = params.delete(:api_format)    || api_format
-                key 	  = params.delete(:api_key)       || api_key
-                timestamp = params.delete(:api_timestamp) || api_timestamp
-                nonce     = params.delete(:api_nonce)     || api_nonce
-                secret    = params.delete(:secret_key)    || secret_key
+            params = params.merge(:api_format    => api_format,
+                                  :api_key       => api_key,
+                                  :api_timestamp => api_timestamp,
+                                  :api_nonce     => api_nonce)
 
-                params = params.merge(:api_format    => api_format,
-                                      :api_key       => api_key,
-                                      :api_timestamp => api_timestamp,
-                                      :api_nonce     => api_nonce)
+            http.get(url, params.merge(:api_signature => self.signature(params)))
+        end
 
-                http.get(url, params.merge(:api_signature => self.signature(params)))
-            end
+        def post_request(options = {})
+            params = options.dup
 
-            def post_request(options = {})
-                params = options.dup
+            http      = params.delete(:client)        || client
+            url       = params.delete(:api_url)       || api_url
+            format    = params.delete(:api_format)    || api_format
+            key 	  = params.delete(:api_key)       || api_key
+            timestamp = params.delete(:api_timestamp) || api_timestamp
+            nonce     = params.delete(:api_nonce)     || api_nonce
+            secret    = params.delete(:secret_key)    || secret_key
 
-                http      = params.delete(:client)        || client
-                url       = params.delete(:api_url)       || api_url
-                format    = params.delete(:api_format)    || api_format
-                key 	  = params.delete(:api_key)       || api_key
-                timestamp = params.delete(:api_timestamp) || api_timestamp
-                nonce     = params.delete(:api_nonce)     || api_nonce
-                secret    = params.delete(:secret_key)    || secret_key
+            params = params.merge(:api_format    => api_format,
+                                  :api_key       => api_key,
+                                  :api_timestamp => api_timestamp,
+                                  :api_nonce     => api_nonce)
 
-                params = params.merge(:api_format    => api_format,
-                                      :api_key       => api_key,
-                                      :api_timestamp => api_timestamp,
-                                      :api_nonce     => api_nonce)
-
-                http.post(url, params.merge(:api_signature => self.signature(params)))
-            end
-
+            http.post(url, params.merge(:api_signature => self.signature(params)))
         end
 
 	end
