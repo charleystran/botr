0d5af0314f73bb4c1fdd548246fa7fe2061025db
bertrandk <b.karerangabo@gmail.com>
2013-08-05 01:43:10 -0400

    Add configuration capabilties.

---
+++ lib/botr/configuration.rb
@@ -0,0 +1,18 @@
+module BOTR
+
+	class << self
+    	attr_accessor :configuration
+  	end
+
+  	def self.configure
+    	self.configuration ||= Configuration.new
+    	yield(configuration)
+  	end
+
+  	class Configuration
+
+  		attr_accessor :protocol, :server, :key, :secret_key
+
+  	end
+
+end
