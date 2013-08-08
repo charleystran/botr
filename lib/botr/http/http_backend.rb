3a3613679412a928f86f2d7d4f86ebb200dbdfb9
bertrandk <b.karerangabo@gmail.com>
2013-08-07 21:17:59 -0400

    Handle multipart data creation in its own object.

--- lib/botr/http/http_backend.rb
+++ lib/botr/http/http_backend.rb
@@ -1,5 +1,4 @@
 require 'net/http'
-require 'mime/types'
 
 module BOTR
 
@@ -13,6 +12,7 @@ module BOTR
 			res = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
 				http.request_get uri
 			end
+			
 			respond(res)
 		end
 
@@ -22,17 +22,27 @@ module BOTR
 			uri.query = URI.encode_www_form(params)
 
 			res = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+				req = Net::HTTP::Post.new(uri.request_uri)
+
 				if data_path.empty?
-					http.post_form(uri, params)
+					req.set_form_data(params)
 				else
-					post_multipart(http, uri, data_path)
+					boundary = rand(1000000).to_s
+					
+					req.body_stream = BOTR::Multipart.new(data_path, boundary)
+					req["Content-Type"] = "multipart/form-data; boundary=#{boundary}"
+					req["Content-Length"] = req.body_stream.size.to_s		
 				end
+				
+				http.request req
 			end
-			respond(res)
+
+			respond(res)			
 		end		
 
 		private
 
+			# Map Net::HTTPResponses to internal ones.
 			def respond(resp)
 				case resp
 	            when Net::HTTPOK
@@ -51,57 +61,6 @@ module BOTR
 	                BOTR::HTTPResponse.new(resp.code, resp.body)
 	            end
 			end
-
-			def post_multipart(http, uri, data_path)
-				boundary = "--BitsOnTheRunMultipartBoundary--" + rand(1000000).to_s
-				
-				req = Net::HTTP::Post.new(uri)
-				req["Content-Type"] = "multipart/form-data, boundary=#{boundary}"
-				req.body_stream = build_stream(boundary, data_path)
-				req["Content-Length"] = req.body_stream.size
-
-				http.request(req)
-			end
-
-			def build_stream(boundary, data_path)
-				name = File.basename(data_path, File.extname(data_path))
-				content_length = File.size(data_path)
-				content_type = MIME::Types.type_for(data_path).first.content_type
-				
-				headIO = build_head(boundary, name, data_path, content_type, content_length)
-				begin
-					bodyIO = File.open(data_path)
-				rescue Exception => e
-					raise #{e}
-				end
-				tailIO = build_tail(boundary)
-
-				BOTR::MultipartIO.new(headIO, bodyIO, tailIO)
-			end
-
-			def build_head(boundary, name, data_path, content_type, content_length)
-				transfer_encoding = "binary"
-				content_disposition = "form-data"
-
-				head = ""
-				head << "--#{boundary}\r\n"
-				head << "Content-Disposition: #{content_disposition}; name=\"#{name}\"; filename=\"#{data_path}\"\r\n"
-				head << "Content-Length: #{content_length}\r\n"
-				head << "Content-Type: #{content_type}\r\n"
-				head << "Content-Transfer-Encoding: #{transfer_encoding}\r\n"
-				head << "\r\n"
-
-				StringIO.new(head)
-			end
-
-			def build_tail(boundary)
-				tail = ""
-				tail << "\r\n\r\n"
-				tail << "--#{boundary}--\r\n\r\n"
-
-				StringIO.new(tail)
-			end
-
 	end
 
 end
