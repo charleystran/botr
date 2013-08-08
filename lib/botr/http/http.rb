336103c07b3861c1b361ca87f55694e4a3fbb133
bertrandk <b.karerangabo@gmail.com>
2013-08-07 21:16:11 -0400

    Fix post_request call with appropriate parameters.

--- lib/botr/http/http.rb
+++ lib/botr/http/http.rb
@@ -2,50 +2,46 @@ module BOTR
 
 	module HTTP
 
-        def client
-            @client = BOTR::HTTPBackend.new unless @client
-            @client
-        end
+		def client
+			@client = BOTR::HTTPBackend.new unless @client
+			@client
+		end
 
-        def get_request(options = {})
-            params = options.dup
+		def get_request(options = {})
+			params = options.dup
 
-            http      = params.delete(:client)        || client
-            method    = params.delete(:method)
-            url       = params.delete(:api_url)       || api_url(method)
-            format    = params.delete(:api_format)    || api_format
-            key       = params.delete(:api_key)       || api_key
-            timestamp = params.delete(:api_timestamp) || api_timestamp
-            nonce     = params.delete(:api_nonce)     || api_nonce
-            secret    = params.delete(:api_secret_key)|| api_secret_key
+			http      = params.delete(:client)        || client
+			method    = params.delete(:method)
+			url       = params.delete(:api_url)       || api_url(method)
+			format    = params.delete(:api_format)    || api_format
+			key       = params.delete(:api_key)       || api_key
+			timestamp = params.delete(:api_timestamp) || api_timestamp
+			nonce     = params.delete(:api_nonce)     || api_nonce
+			secret    = params.delete(:api_secret_key)|| api_secret_key
 
-            params = params.merge(:api_format    => format,
-                                  :api_key       => key,
-                                  :api_timestamp => timestamp,
-                                  :api_nonce     => nonce)
+			params = params.merge(:api_format    => format,
+								  :api_key       => key,
+								  :api_timestamp => timestamp,
+								  :api_nonce     => nonce)
 
-            http.get(url, params.merge(:api_signature => self.signature(params)))
-        end
+			http.get(url, params.merge(:api_signature => self.signature(params)))
+		end
 
-        def post_request(options = {}, data_path = nil)
-            params = options.dup
+		def post_request(options = {}, data_path = nil)
+			params = options.dup
 
-            http      = params.delete(:client)        || client
-            method    = params.delete(:method)
-            url       = params.delete(:api_url)       || api_url(method)
-            format    = params.delete(:api_format)    || api_format
-            key       = params.delete(:api_key)       || api_key
-            timestamp = params.delete(:api_timestamp) || api_timestamp
-            nonce     = params.delete(:api_nonce)     || api_nonce
-            secret    = params.delete(:api_secret_key)|| api_secret_key
+			http      = params.delete(:client)        || client
+			url       = params.delete(:upload_url)    || upload_url
+			format    = params.delete(:api_format)    || api_format
+			key       = params.delete(:upload_key)    || @upload_key
+			token     = params.delete(:upload_token)  || @upload_token
 
-            params = params.merge(:api_format    => format,
-                                  :api_key       => key,
-                                  :api_timestamp => timestamp,
-                                  :api_nonce     => nonce)
+			params = params.merge(:api_format    => format,
+								  :key           => key,
+								  :token         => token)
 
-            http.post(url, params.merge(:api_signature => self.signature(params)), data_path)
-        end
+			http.post(url, params, data_path)
+		end
 
 	end
 
