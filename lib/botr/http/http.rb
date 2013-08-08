950efc125c4890275772a3620a2a92e41a0728ec
bertrandk <b.karerangabo@gmail.com>
2013-08-08 01:26:33 -0400

    Add support for PUT requests (updates).

--- lib/botr/http/http.rb
+++ lib/botr/http/http.rb
@@ -43,11 +43,31 @@ module BOTR
 			http.post(url, params, data_path)
 		end
 
+		def put_request(options = {})
+			params = options.dup
+
+			http      = params.delete(:client)        || client
+			method    = 'update'
+			url       = params.delete(:api_url)       || api_url(method)
+			format    = params.delete(:api_format)    || api_format
+			key       = params.delete(:api_key)       || api_key
+			timestamp = params.delete(:api_timestamp) || api_timestamp
+			nonce     = params.delete(:api_nonce)     || api_nonce
+			secret    = params.delete(:api_secret_key)|| api_secret_key
+
+			params = params.merge(:api_format    => format,
+								  :api_key       => key,
+								  :api_timestamp => timestamp,
+								  :api_nonce     => nonce)
+
+			http.post(url, params.merge(:api_signature => self.signature(params)))
+		end
+
 		def delete_request(options = {})
 			params = options.dup
 
 			http      = params.delete(:client)        || client
-			method    = params.delete(:method)
+			method    = 'delete'
 			url       = params.delete(:api_url)       || api_url(method)
 			format    = params.delete(:api_format)    || api_format
 			key       = params.delete(:api_key)       || api_key
